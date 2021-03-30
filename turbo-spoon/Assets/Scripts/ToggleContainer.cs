7078f9f36bc2f2ed5df3ea662b4d607af0640f95
mzimm23 <mzimm23@lsu.edu>
2021-03-30 15:41:50 -0500

    added tweening to menu and attempted to toggle plane visualization

--- turbo-spoon/Assets/Scripts/ToggleContainer.cs
+++ turbo-spoon/Assets/Scripts/ToggleContainer.cs
@@ -7,7 +7,21 @@ public class ToggleContainer : MonoBehaviour
 {
 
     [SerializeField]
+    GameObject scrollContainer;
+
+    [SerializeField]
     public GameObject[] containers;
+
+    [SerializeField]
+    GameObject myCanvas;
+
+    private bool isScrollContOnScreen = true;
+    private float scrollContStartY;
+    float screenBottom;
+
+    public GameObject plane;
+
+
 
     public void ToggleActive()
     {
@@ -22,5 +36,41 @@ public class ToggleContainer : MonoBehaviour
                 obj.SetActive(true);
             }
         }
+    }
+
+    public void togglePlane()
+    {
+        if (plane.activeSelf == true)
+        {
+            plane.SetActive(false);
+        }
+        else
+        {
+            plane.SetActive(true);
+        }
+    }
+
+    public void TweenDown(GameObject obj)
+    {
+        float tweenTime = 0.25f;
+        if (isScrollContOnScreen == true)
+        {
+            isScrollContOnScreen = false;
+            LeanTween.cancel(obj);
+            LeanTween.moveLocalY(obj, (screenBottom - 200), tweenTime);
+        }
+        else
+        {
+            isScrollContOnScreen = true;
+            LeanTween.cancel(obj);
+            LeanTween.moveLocalY(obj, scrollContStartY, tweenTime);
+        }
+    }
+
+    private void Start()
+    {
+        scrollContStartY = scrollContainer.transform.localPosition.y;
+        screenBottom = myCanvas.GetComponent<RectTransform>().rect.top;
+        plane = GameObject.FindGameObjectWithTag("Plane");
     }
 }
