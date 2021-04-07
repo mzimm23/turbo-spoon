bfa33d85ca330a47478715426514cf39ba844f10
mzimm23 <mzimm23@lsu.edu>
2021-04-06 22:30:47 -0500

    Changed the way the menu selection works

--- turbo-spoon/Assets/ChangeItems.cs
+++ turbo-spoon/Assets/ChangeItems.cs
@@ -6,57 +6,20 @@ using UnityEngine.UI;
 public class ChangeItems : MonoBehaviour
 {
     [SerializeField]
-    public Button[] abstractArt, second, menu;
-    public Button[] activeButtons;
+    public GameObject activeButton;
 
 
     // Start is called before the first frame update
     void Start()
     {
-       foreach(var btn in abstractArt)
-        {
-            btn.gameObject.SetActive(true);
-        }
-        activeButtons = abstractArt;
+        
     }
 
-    public void EnableSecond()
+    public void ChangeMenu(GameObject newObj)
     {
-        foreach (var btn in activeButtons)
-        {
-            btn.gameObject.SetActive(false);
-        }
-        foreach (var btn in second)
-        {
-            btn.gameObject.SetActive(true);
-        }
-        activeButtons = second;
-    }
-
-    public void EnableAbstract()
-    {
-        foreach (var btn in activeButtons)
-        {
-            btn.gameObject.SetActive(false);
-        }
-        foreach (var btn in abstractArt)
-        {
-            btn.gameObject.SetActive(true);
-        }
-        activeButtons = abstractArt;
-    }
-
-    public void EnableMenu()
-    {
-        foreach (var btn in activeButtons)
-        {
-            btn.gameObject.SetActive(false);
-        }
-        foreach (var btn in menu)
-        {
-            btn.gameObject.SetActive(true);
-        }
-        activeButtons = menu;
+        activeButton.SetActive(false);
+        newObj.SetActive(true);
+        activeButton = newObj;
     }
 
 }
